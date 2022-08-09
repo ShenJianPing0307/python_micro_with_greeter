10bd92bf4a0b2034ff44ca80773d1c6ede04859d
ShenJianPing0307 <610502705@qq.com>
2022-08-10 07:11:05 +0800

    web层加入nacos配置中心

--- greeter_web/config/config.py
+++ greeter_web/config/config.py
@@ -1,4 +1,6 @@
 import socket
+import nacos
+import json
 
 
 def get_free_tcp_port():
@@ -9,16 +11,46 @@ def get_free_tcp_port():
     return port
 
 
+NACOS = {
+    "Host": "192.168.111.5",
+    "Port": 8848,
+    "NameSpace": "0a6d3e81-e929-4afe-9771-0eb4dbcda6ef",
+    "User": "nacos",
+    "Password": "nacos",
+    "DataId": "greeter_web",
+    "Group": "dev"
+}
+client = nacos.NacosClient(f'{NACOS["Host"]}:{NACOS["Port"]}', namespace=NACOS["NameSpace"],
+                           username=NACOS["User"],
+                           password=NACOS["Password"])
+
+# client取出的数据是json字符串
+config_data = client.get_config(NACOS["DataId"], NACOS["Group"])
+config_data = json.loads(config_data)
+"""
+nacos中配置的json数据格式：
+{
+    "CONSUL_HOST":"192.168.111.5",
+    "CONSUL_PORT":8500,
+    "SERVICE_HOST":"192.168.0.118",
+    "SERVICE_PORT":8008,
+    "SERVICE_NAME":"greeter_api",
+    "SERVICE_TAGS":["api"],
+    "CONSUL_FILTER_KEY":"Service",
+    "CONSUL_FILTER_VALUE":"greeter_srv"
+}
+"""
+
 # Consul info
-CONSUL_HOST = '192.168.111.5'
-CONSUL_PORT = 8500
+CONSUL_HOST = config_data['CONSUL_HOST']
+CONSUL_PORT = int(config_data['CONSUL_PORT'])
 
 # Server info
-SERVICE_HOST = '192.168.0.118'
-SERVICE_PORT = get_free_tcp_port() | 8008
-SERVICE_NAME = "greeter_api"
-SERVICE_TAGS = ["api"]
+SERVICE_HOST = config_data['SERVICE_HOST']
+SERVICE_PORT = get_free_tcp_port() | int(config_data['SERVICE_PORT'])
+SERVICE_NAME = config_data['SERVICE_NAME']
+SERVICE_TAGS = config_data['SERVICE_TAGS']
 
 # Consul key
-CONSUL_FILTER_KEY = 'Service'
-CONSUL_FILTER_VALUE = 'greeter_srv'
+CONSUL_FILTER_KEY = config_data['CONSUL_FILTER_KEY']
+CONSUL_FILTER_VALUE = config_data['CONSUL_FILTER_VALUE']
